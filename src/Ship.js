4bf781040b867291201debc389a1e01768a8990e
skluber <arodriguezzmartinez@gmail.com>
2026-09-14 16:19:08 +0200

    Add Ship hit and sink behavior

--- src/Ship.js
+++ src/Ship.js
@@ -4,5 +4,17 @@ const Ship = (length) => {
     return {
         length,
         hits: 0,
+
+        hit() {
+            if (this.isSunk()) {
+                return;
+            } else {
+                this.hits++;
+            }
+        },
+
+        isSunk() {
+            return this.length === this.hits;
+        }
     }
 }
--- tests/Ship.test.js
+++ tests/Ship.test.js
@@ -5,3 +5,21 @@ test("creates a ship with the given length", () => {
 
     expect(ship.length).toBe(3);
 });
+
+test("hitting a ship increases its hit count", () => {
+    const ship = Ship(3);
+
+    ship.hit();
+
+    expect(ship.hits).toBe(1);
+});
+
+test("ship gests sunk when hits reach its length", () => {
+    const ship = Ship(3);
+
+    ship.hit();
+    ship.hit();
+    ship.hit();
+
+    expect(ship.isSunk()).toBe(true);
+});

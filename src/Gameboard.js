5d9bd953b42660344e0be82fe3e17f0240c601d3
skluber <arodriguezzmartinez@gmail.com>
2026-09-14 17:20:26 +0200

    Add ship placement to Gameboard

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -5,5 +5,28 @@ const Gameboard = () => {
         cells: Array(10).fill(null).map(() => {
             return Array(10).fill(null);
         }),
+
+        placeShip(ship, [x, y], orientation) {
+            if (ship.length <= 0) return;
+            if (ship.length + x > 10 && orientation === "horizontal") throw new Error("Ship is out of board");
+            if (ship.length + y > 10 && orientation === "vertical") throw new Error("Ship is out of board");
+            if (orientation !== "horizontal" && orientation !== "vertical") throw new Error("Orientation is not supported");
+
+            for (let i = 0; i < ship.length; i++) {
+                if (orientation === "horizontal") {
+                    if (this.cells[y][x + i] !== null) throw new Error("Already a ship in position");
+                } else if (orientation === "vertical") {
+                    if (this.cells[y + i][x] !== null) throw new Error("Already a ship in position");
+                }
+            }
+
+            for (let i = 0; i < ship.length; i++) {
+                if (orientation === "horizontal") {
+                    this.cells[y][x + i] = ship;
+                } else if (orientation === "vertical") {
+                    this.cells[y + i][x] = ship;
+                }
+            }
+        }
     }
 }
--- tests/Gameboard.test.js
+++ tests/Gameboard.test.js
@@ -1,8 +1,60 @@
 import { Gameboard } from "../src/Gameboard";
+import { Ship } from "../src/Ship";
 
 test("creates a 10x10 gameboard", () => {
     const gameboard = Gameboard();
 
     expect(gameboard.cells.length).toBe(10);
     expect(gameboard.cells[0].length).toBe(10);
+}) 
+
+test("places a ship horizontally", () => {
+    const gameboard = Gameboard();
+    const ship = Ship(3)
+
+    gameboard.placeShip(ship, [2, 4], "horizontal");
+
+    expect(gameboard.cells[4][2]).toBe(ship);
+    expect(gameboard.cells[4][3]).toBe(ship);
+    expect(gameboard.cells[4][4]).toBe(ship);
+}) 
+
+test("places a ship vertically", () => {
+    const gameboard = Gameboard();
+    const ship = Ship(3)
+
+    gameboard.placeShip(ship, [2, 4], "vertical");
+
+    expect(gameboard.cells[4][2]).toBe(ship);
+    expect(gameboard.cells[5][2]).toBe(ship);
+    expect(gameboard.cells[6][2]).toBe(ship);
+}) 
+
+test("doesn't place a ship out of board", () => {
+    const gameboard = Gameboard();
+    const ship = Ship(3)
+
+    expect(() => {
+        gameboard.placeShip(ship, [2, 8], "vertical")
+    }).toThrow("Ship is out of board");
+}) 
+
+test("doesn't place a ship on an occupied position", () => {
+    const gameboard = Gameboard();
+    const ship = Ship(3)
+    const ship2 = Ship(3)
+
+    expect(() => {
+        gameboard.placeShip(ship, [2, 2], "vertical");
+        gameboard.placeShip(ship2, [2, 2], "vertical");
+    }).toThrow("Already a ship in position");
+}) 
+
+test("checks supported orientations", () => {
+    const gameboard = Gameboard();
+    const ship = Ship(3)
+
+    expect(() => {
+        gameboard.placeShip(ship, [2, 2], "diagonal");
+    }).toThrow("Orientation is not supported");
 }) 
